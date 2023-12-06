be154f889842c7a105d1ff50f02b2e3f8c4bdb18
jorge robayo <jorgerobmor@gmail.com>
2023-12-06 00:58:12 -0500

    Added what is info to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,40 @@ function Home() {
     <div>
       <div className="homeInfo">
         <h1>Game Finder</h1>
-        <p>
-          Welcome to Game Finder.
-        </p>
+
+        <img
+          className="homeImg"
+          src={
+            "https://wallpapers.com/images/hd/80s-aesthetic-dcinaahccs5hc4nn.jpg"
+          }
+          alt=""
+        />
+
+        <section className="whtIsGmFndr">
+          <h3>What is Game Finder</h3>
+          <p>
+            Here you will be able to search for any game you could have ever
+            imagined. We have all relevant information that you need!
+          </p>
+
+            <ul className="relevantInfoList">
+              <div>
+                <li>~ Name</li>
+                <li>~ Platforms</li>
+                <li>~ Generes</li>
+              </div>
+
+              <div>
+                <li>~ Stores</li>
+                <li>~ Released Date</li>
+                <li>~ Game Screenshots</li>
+              </div>
+            </ul>
+          </section>
+
+        <section>
+          <h3>Why Game Finder</h3>
+        </section>
       </div>
     </div>
   );
