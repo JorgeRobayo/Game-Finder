9ae2d49f7e72980f0ef9d9eed3f655eb7d71f205
jorge robayo <jorgerobmor@gmail.com>
2023-12-07 10:38:10 -0500

    Game info is now being displayed properly

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,36 +1,57 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { dataContext } from "../components/Context";
 
 function GameDetails(props) {
   // const { game, setGame } = useState(null);
 
-  const {currentData, setCurrentData} = useContext(dataContext)
+  const { currentData, setCurrentData } = useContext(dataContext);
 
   return (
     <div>
-      <h1>{currentData.name}</h1>
+      <section className="gmName">
+        <h1>{currentData.name}</h1>
+      </section>
 
-      {/* <h2>Released</h2>
-      <p>{currentData.released}</p> */}
-
-      <h2>Rating</h2>
-      <p>{currentData.rating}</p>
-
-      {/* <h3>Genere(s)</h3>
-      {currentData.genres.map((g) => `${g.name} |`)} */}
-
-      <h3>Platform(s)</h3>
-      {currentData.platforms.map((p) => `${p.platform.name}`)}
-
-      {/* <h3>Game Screenshots</h3>
-      <ul>
+      <h3>Game Screenshots</h3>
+      <section>
         {currentData.short_screenshots.map((ss) => (
-          <li>
-            <img src={ss.image} alt="screenshot" />
-          </li>
+          <div className="gmScrshtDiv">
+            <img className="gmScrsht" src={ss.image} alt="screenshot" />
+          </div>
         ))}
-      </ul> */}
+      </section>
+      
+      <section className="gmRlsdDate">
+        <h3>Released</h3>
+        <p>{currentData.released}</p>
+      </section>
+
+      <section className="gmRtng">
+        <h3>Rating</h3>
+        <p>{currentData.rating}</p>
+      </section>
+
+      <section className="gmGnrs">
+        <h3>Genere(s)</h3>
+        <div className="gmGnrsMap">
+          {currentData.genres.map((g) => `${g.name} | `)}
+        </div>
+      </section>
+
+      <section className="gmPltfrm">
+        <h3>Platform(s)</h3>
+        <div className="gmPltfrmMap">
+          {currentData.platforms.map((p) => `${p.platform.name}`)}
+        </div>
+      </section>
+
+      <section className="gmStrs">
+        <h3>Stores</h3>
+        <div className="gmStrsMap">
+          {currentData.stores.map((str) => `${str.store.name} | `)}
+        </div>
+      </section>
     </div>
   );
 }
