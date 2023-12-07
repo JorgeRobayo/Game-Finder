4b44202bd1e82673a1fa09eb8631e8a990504306
jorge robayo <jorgerobmor@gmail.com>
2023-12-07 16:23:54 -0500

    added carousel but its not displaying properly

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,34 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { dataContext } from "./Context";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function Results(props) {
   const { currentData, setCurrentData } = useContext(dataContext);
 
+  //this object tells the responsivenes of the carousel
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div >
-      <ul className="gameContainer">
-        {props.gameResults.map((game) => (
-          <li key={game.id}>
-            <Link
-              to="/gamedetails"
-              onClick={() => {
-                setCurrentData(game);
-                console.log(currentData);
-              }}
-            >
-              <section>
+    <div>
+      <section className="gameContainer">
+        <Carousel responsive={responsive}>
+          {props.gameResults.map((game) => (
+            <div key={game.id}>
+              <Link
+                to="/gamedetails"
+                onClick={() => {
+                  setCurrentData(game);
+                  console.log(currentData);
+                }}
+              >
+                <section>
                   <img
                     className="gameImg"
                     src={game.background_image}
                     alt="game"
                   />
                   <h3>{game.name}</h3>
-              </section>
-            </Link>
-          </li>
-        ))}
-      </ul>
+                </section>
+              </Link>
+            </div>
+          ))}
+        </Carousel>
+      </section>
     </div>
   );
 }
