05ed14f1ee263aeb9fc28ee5d63836c1e7344c29
jorge robayo <jorgerobmor@gmail.com>
2023-12-09 22:10:13 -0500

    Just uncommented the second fetch request, I am changing my first chanfge request to await/async

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,46 +18,39 @@ function SearchBar() {
     let slug = searchTerm.split(" ").join("-").toLowerCase();
 
     setGameResults([]);
-    fetch(
-      `https://api.rawg.io/api/games?search=${slug}&key=270a250046034f97a6940f3241a34200`
-    )
-      .then((resp) => resp.json())
-      .then(({ results }) => {
-        results === undefined
-          ? alert("No games found")
-          : setGameResults(results);
-      });
+    
+    
 
     setSearchTerm("");
   };
   
-  // const handleGame = async (game) => {
-  //   const detailedInfo = await getGmDescription(game.Id)
+  const handleGame = async (game) => {
+    const detailedInfo = await getGmDescription(game.Id)
 
-  //   setCurrentData(detailedInfo)
-  //   console.log('gameinfo', detailedInfo)
-  //   console.log('hello world')
-  // }
+    setCurrentData(detailedInfo)
+    console.log('gameinfo', detailedInfo)
+    console.log('hello world')
+  }
 
-  // const getGmDescription = async (gameId) => {
-  //   const url = `https://api.rawg.io/api/games/${gameId}?key=270a250046034f97a6940f3241a34200`;
+  const getGmDescription = async (gameId) => {
+    const url = `https://api.rawg.io/api/games/${gameId}?key=270a250046034f97a6940f3241a34200`;
 
-  //   try {
-  //     const response = await fetch(url);
+    try {
+      const response = await fetch(url);
 
-  //     if (!response.ok){
-  //       throw new Error(`request error ${response.status}`)
-  //     }
-  //     const gameDescResult = await response.json();
+      if (!response.ok){
+        throw new Error(`request error ${response.status}`)
+      }
+      const gameDescResult = await response.json();
 
-  //     console.log('API response', gameDescResult)
+      console.log('API response', gameDescResult)
 
-  //     return gameDescResult;
-  //     }catch (error) {
-  //       console.error('error:', error)
-  //       return null
-  //     }
-  // };
+      return gameDescResult;
+      }catch (error) {
+        console.error('error:', error)
+        return null
+      }
+  };
  
   console.log(gameResults);
  
