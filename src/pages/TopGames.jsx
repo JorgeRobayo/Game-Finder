f8288939c50f11636636fbbead5b39dc96521ecd
jorge robayo <jorgerobmor@gmail.com>
2023-12-09 23:54:34 -0500

    Adding styling to top games and info

--- src/pages/TopGames.jsx
+++ src/pages/TopGames.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function TopGames() {
   return (
     <div>
-        <h1>Top games of all time</h1>
+      <section className='topGmsTitle'>
+        <h1>Top Games of all Time</h1>
+      </section>
+
+      <section id='game1' className='topGames'>
+      
+      </section>
+        
     </div>
   )
 }
