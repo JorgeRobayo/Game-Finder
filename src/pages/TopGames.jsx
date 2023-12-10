d8ccb08e928dc4a87b971ed1c41b6e4001ca982a
jorge robayo <jorgerobmor@gmail.com>
2023-12-10 00:49:30 -0500

    added a secondd article in top games

--- src/pages/TopGames.jsx
+++ src/pages/TopGames.jsx
@@ -31,24 +31,60 @@ function TopGames() {
             enemies. There are five historically based campaigns, which
             conscript the player to specialized and story-backed conditions, as
             well as three additional single-player game modes; multiplayer is
-            also supported. 
+            also supported.
           </h3>
-
         </div>
       </section>
-          <h3 className="gmDescription">
-          Reception of Age of Empires II was highly positive.
-            The significant number of new features was praised, as were the
-            gameplay improvements. The game received "universal acclaim"
-            according to video game review aggregator Metacritic. Three months
-            after its release, two million copies of Age of Empires II had been
-            shipped, and it topped sales charts in seven countries. The game won
-            multiple awards and is today considered a classic of its type,
-            having had a significant impact on future games in its genre. The
-            original Age of Empires II and its 2000 expansion pack, The
-            Conquerors, were later released as The Gold Edition. Age of Empires
-            II is often considered one of the greatest games ever made.
+
+      <section>
+        <h3 id="game1Description2" className="gmDescription">
+          Reception of Age of Empires II was highly positive. The significant
+          number of new features was praised, as were the gameplay improvements.
+          The game received "universal acclaim" according to video game review
+          aggregator Metacritic. Three months after its release, two million
+          copies of Age of Empires II had been shipped, and it topped sales
+          charts in seven countries. The game won multiple awards and is today
+          considered a classic of its type, having had a significant impact on
+          future games in its genre. The original Age of Empires II and its 2000
+          expansion pack, The Conquerors, were later released as The Gold
+          Edition. Age of Empires II is often considered one of the greatest
+          games ever made.
+        </h3>
+      </section>
+
+      <h2 className="gmTitle">Super Mario Bros (1985)</h2>
+
+      <section id="game1" className="topGamesReversed">
+        <div className="topGmImgDiv">
+          <img
+            className="topGmImg"
+            src={
+              "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
+            }
+            alt=""
+          />
+        </div>
+
+        <div className="gmDescription">
+          <h3>
+            Players control Mario, or his brother Luigi in the multiplayer mode,
+            as they traverse the Mushroom Kingdom to rescue Princess Toadstool
+            from King Koopa. They traverse side-scrolling stages while avoiding
+            hazards such as enemies and pits with the aid of power-ups such as
+            the Super Mushroom, Fire Flower, and Starman.
           </h3>
+
+          <h3>
+            The game was designed by Shigeru Miyamoto and Takashi Tezuka as "a
+            grand culmination" of the Famicom team's three years of game
+            mechanics and programming, drawing from their experiences working on
+            Devil World and the side-scrollers Excitebike and Kung Fu to advance
+            their previous work on platforming "athletic games" such as Donkey
+            Kong and Mario Bros. The design of the first level, World 1-1,
+            serves as a tutorial for platform gameplay.
+          </h3>
+        </div>
+      </section>
     </div>
   );
 }
