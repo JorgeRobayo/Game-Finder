b460812937f4a10ca0aacce3587035cf8fc9f95d
jorge robayo <jorgerobmor@gmail.com>
2023-12-10 22:52:29 -0500

    added description for new game and added header image

--- src/pages/TopGames.jsx
+++ src/pages/TopGames.jsx
@@ -7,6 +7,16 @@ function TopGames() {
         <h1>Top Games of all Time</h1>
       </section>
 
+      <section className="headerImgSection">
+        <img
+          className="headerImg"
+          src={
+            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6fe91322-e36d-4aca-8d83-41904f9e429f/df3kllr-32b45386-cb0b-4d18-8614-d9c5c5349294.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZmZTkxMzIyLWUzNmQtNGFjYS04ZDgzLTQxOTA0ZjllNDI5ZlwvZGYza2xsci0zMmI0NTM4Ni1jYjBiLTRkMTgtODYxNC1kOWM1YzUzNDkyOTQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.wXPGXM3Wy0nOUVmXi1d-CT031ZTvMjTdTsvYShmMXTY"
+          }
+          alt=""
+        />
+      </section>
+
       <h2 className="gmTitle">Age Of Empires II: The Age of Kings (1999)</h2>
 
       <div className="cntnrtRight">
@@ -53,56 +63,16 @@ function TopGames() {
           </h3>
         </section>
       </div>
-      
-      
+
       <h2 className="gmTitle">Super Mario Bros (1985)</h2>
-      
 
       <div className="cntnrReverse">
         <section id="game2" className="topGamesReversed">
-        <div className="topGmImgDiv">
-          <img
-            className="topGmImg"
-            src={
-              "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
-            }
-            alt=""
-          />
-        </div>
-
-        <div className="gmDescription">
-          <h3>
-            Players control Mario, or his brother Luigi in the multiplayer mode,
-            as they traverse the Mushroom Kingdom to rescue Princess Toadstool
-            from King Koopa. They traverse side-scrolling stages while avoiding
-            hazards such as enemies and pits with the aid of power-ups such as
-            the Super Mushroom, Fire Flower, and Starman.
-          </h3>
-
-          <h3>
-            The game was designed by Shigeru Miyamoto and Takashi Tezuka as "a
-            grand culmination" of the Famicom team's three years of game
-            mechanics and programming, drawing from their experiences working on
-            Devil World and the side-scrollers Excitebike and Kung Fu to advance
-            their previous work on platforming "athletic games" such as Donkey
-            Kong and Mario Bros. The design of the first level, World 1-1,
-            serves as a tutorial for platform gameplay.
-          </h3>
-        </div>
-      </section>
-      </div>
-
-
-      <h3 className="gmTitle">Mass Effect (2007)</h3>
-
-      <div className="cntnrtRight">
-       
-      <section id="game3" className="topGames">
           <div className="topGmImgDiv">
             <img
               className="topGmImg"
               src={
-                "https://upload.wikimedia.org/wikipedia/en/5/56/Age_of_Empires_II_-_The_Age_of_Kings_Coverart.png"
+                "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
               }
               alt=""
             />
@@ -110,37 +80,66 @@ function TopGames() {
 
           <div className="gmDescription">
             <h3>
-              Is a real-time strategy video game developed by Ensemble Studios
-              and published by Microsoft. Released in 1999 for Microsoft Windows
-              and Macintosh in 2001, it is the second game in the Age of Empires
-              series. The Age of Kings is set in the Middle Ages and contains
-              thirteen playable civilizations. Players aim to gather resources,
-              which they use to build towns, create armies, and defeat their
-              enemies. There are five historically based campaigns, which
-              conscript the player to specialized and story-backed conditions,
-              as well as three additional single-player game modes; multiplayer
-              is also supported.
+              Players control Mario, or his brother Luigi in the multiplayer
+              mode, as they traverse the Mushroom Kingdom to rescue Princess
+              Toadstool from King Koopa. They traverse side-scrolling stages
+              while avoiding hazards such as enemies and pits with the aid of
+              power-ups such as the Super Mushroom, Fire Flower, and Starman.
+            </h3>
+
+            <h3>
+              The game was designed by Shigeru Miyamoto and Takashi Tezuka as "a
+              grand culmination" of the Famicom team's three years of game
+              mechanics and programming, drawing from their experiences working
+              on Devil World and the side-scrollers Excitebike and Kung Fu to
+              advance their previous work on platforming "athletic games" such
+              as Donkey Kong and Mario Bros. The design of the first level,
+              World 1-1, serves as a tutorial for platform gameplay.
+            </h3>
+          </div>
+        </section>
+      </div>
+
+      <h2 className="gmTitle">Mass Effect (2007)</h2>
+
+      <div className="cntnrtRight">
+        <section id="game3" className="topGames">
+          <div className="topGmImgDiv">
+            <img
+              className="topGmImg"
+              src={
+                "https://upload.wikimedia.org/wikipedia/en/e/e8/MassEffect.jpg"
+              }
+              alt=""
+            />
+          </div>
+
+          <div className="gmDescription">
+            <h3>
+              Planned as the first chapter of a trilogy, Mass Effect was
+              developed over the course of three and a half years, and uses the
+              Unreal Engine 3 as a groundwork. It was designed so that the
+              player would assume the role of a central character that could
+              make important decisions and impact the story of the game in
+              numerous ways. The combat was designed to offer the tactics and
+              customization of a role-playing game, but through a simpler and
+              real-time third-person shooter interface. In 2008 and 2009, two
+              downloadable content packs that introduced new missions to the
+              base game were released.
             </h3>
           </div>
         </section>
 
         <section>
           <h3 id="game1Description2" className="gmDescription">
-            Reception of Age of Empires II was highly positive. The significant
-            number of new features was praised, as were the gameplay
-            improvements. The game received "universal acclaim" according to
-            video game review aggregator Metacritic. Three months after its
-            release, two million copies of Age of Empires II had been shipped,
-            and it topped sales charts in seven countries. The game won multiple
-            awards and is today considered a classic of its type, having had a
-            significant impact on future games in its genre. The original Age of
-            Empires II and its 2000 expansion pack, The Conquerors, were later
-            released as The Gold Edition. Age of Empires II is often considered
-            one of the greatest games ever made.
+            A game that makes you feel good about punching a journalist while
+            also making you feel bad for fancying a polygonal alien, Mass Effect
+            was BioWare's big new punt at a not-Star Wars space opera IP about
+            evil squids destroying the universe. It was great - but the series'
+            best moments were yet to come.
           </h3>
         </section>
       </div>
-      
     </div>
   );
 }
