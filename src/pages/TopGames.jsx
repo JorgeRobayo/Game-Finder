2f5e0565c5f9dbae5b37c2d3951df342bf9a2f13
jorge robayo <jorgerobmor@gmail.com>
2023-12-10 23:27:47 -0500

    added description for fortnite and added a new game continer

--- src/pages/TopGames.jsx
+++ src/pages/TopGames.jsx
@@ -256,9 +256,7 @@ function TopGames() {
               <img
                 id="forniteImg3"
                 className="topGmImg"
-                src={
-                  "https://i.ytimg.com/vi/OiG8DuoHVVk/maxresdefault.jpg"
-                }
+                src={"https://i.ytimg.com/vi/OiG8DuoHVVk/maxresdefault.jpg"}
                 alt=""
               />
             </div>
@@ -266,33 +264,71 @@ function TopGames() {
 
           <div className="gmDescription">
             <h3>
-              Grand Theft Auto V is a 2013 action-adventure game developed by
-              Rockstar North and published by Rockstar Games. It is the seventh
-              main entry in the Grand Theft Auto series, following 2008's Grand
-              Theft Auto IV, and the fifteenth instalment overall. Set within
-              the fictional state of San Andreas, based on Southern California,
-              the single-player story follows three protagonists—retired bank
-              robber Michael De Santa, street gangster Franklin Clinton, and
-              drug dealer and gunrunner Trevor Philips—and their attempts to
-              commit heists while under pressure from a corrupt government
-              agency and powerful criminals. The open world design lets players
-              freely roam San Andreas's open countryside and the fictional city
-              of Los Santos, based on Los Angeles.
+              Fortnite is an online video game and game platform developed by
+              Epic Games and released in 2017. It is available in six distinct
+              game mode versions that otherwise share the same general gameplay
+              and game engine: Fortnite Battle Royale, a free-to-play battle
+              royale game in which up to 100 players fight to be the last person
+              standing; Fortnite: Save the World, a cooperative hybrid tower
+              defense-shooter and survival game in which up to four players
+              fight off zombie-like creatures and defend objects with traps and
+              fortifications they can build; and Fortnite Creative, in which
+              players are given complete freedom to create worlds and battle
+              arenas, Lego Fortnite, Rocket Racing and Fortnite Festival.
             </h3>
 
             <h3>
-              Grand Theft Auto V is an action-adventure game played from either
-              a third-person or first-person perspective. Players complete
-              missions—linear scenarios with set objectives—to progress through
-              the story. Outside of the missions, players may freely roam the
-              open world. Composed of the San Andreas open countryside area,
-              including the fictional Blaine County, and the fictional city of
-              Los Santos, the world is much larger in area than earlier entries
-              in the series. It may be fully explored after the game's beginning
-              without restriction, although story progress unlocks more gameplay
-              content.
+              A pop culture mash-up machine featuring everything from
+              Christopher Nolan movie trailer debuts to Travis Scott concerts,
+              Fortnite is the closest we've come to a real metaverse in 2023.
+              For kids growing up nowadays, it's practically a rite of passage.
             </h3>
           </div>
+        </section>
+      </div>
+
+      <h2 className="gmTitle">Tony Hawk's Pro Skater 2 (2000)</h2>
+
+      <div className="cntnrtRight">
+        <section id="game3" className="topGames">
+          <div className="topGmImgDiv">
+            <img
+              className="topGmImg"
+              src={
+                "https://upload.wikimedia.org/wikipedia/en/a/a3/Official_cover_art_for_Bioshock_Infinite.jpg"
+              }
+              alt=""
+            />
+          </div>
+
+          <div className="gmDescription">
+            <h3>
+              BioShock Infinite is a first-person shooter video game in the
+              BioShock series, developed by Irrational Games and published by
+              2K. Infinite was released worldwide for the PlayStation 3,
+              Windows, Xbox 360, and OS X platforms in 2013. The game is set in
+              the year 1912 and follows its protagonist, Booker DeWitt, who is
+              sent to the airborne city Columbia to retrieve Elizabeth, a young
+              woman held captive there. Booker and Elizabeth become involved in
+              a class war between the nativist Founders that rule Columbia and
+              the rebel Vox Populi, representing the city's underclass.
+              Elizabeth possesses the ability to manipulate "Tears" in the
+              space-time continuum, and Booker and Elizabeth discover she is
+              central to Columbia's dark secrets. The player controls Booker
+              DeWitt throughout the game, fighting enemies and scavenging
+              supplies, while the computer-controlled Elizabeth provides
+              assistance.
+            </h3>
+          </div>
+        </section>
+
+        <section>
+          <h3 id="game1Description2" className="gmDescription">
+            There are entire childhoods with just one core memory: trying to get
+            a 1080 in the halfpipe in the Hangar level for absolutely no reward
+            bar your own sense of accomplishment. Wouldn't have it any other
+            way.
+          </h3>
         </section>
       </div>
     </div>
